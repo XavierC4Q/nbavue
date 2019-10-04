278451cb470d335ec3a6e85c95e9d9e798fcb0fb
Xavier <munroexavier@gmail.com>
2019-10-03 23:14:17 -0400

    adding vuetify. gonna style this up quick over the weekend.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,36 @@
 <template>
-    <router-view />
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
+<script>
+import HelloWorld from './components/HelloWorld';
 
-</style>
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
